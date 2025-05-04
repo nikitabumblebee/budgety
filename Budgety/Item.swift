0b3f8e894490e37c48efd61b6a2a6756a67ddcb3
NikitaBumblebee <nikitashmelev7@gmail.com>
2025-05-04 10:46:10 +0700

    [BUD-1] Initial commit

---
+++ Budgety/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Budgety
+//
+//  Created by Nikita Shmelev on 04.05.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ BudgetyTests/BudgetyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetyTests.swift
+//  BudgetyTests
+//
+//  Created by Nikita Shmelev on 04.05.2025.
+//
+
+import Testing
+@testable import Budgety
+
+struct BudgetyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
